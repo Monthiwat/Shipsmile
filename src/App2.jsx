f125e88b62c0c48f45ca40d72936ba3c2292e6e0
vinai.b <vinai.b@sabuysolutions.com>
2023-11-18 15:31:02 +0700

    [update]

--- src/App2.jsx
+++ src/App2.jsx
@@ -61,6 +61,56 @@ function App() {
       </div>
     </main>
   );
+  // return (
+  //   <>
+  //     <div
+  //       className="hero min-h-screen"
+  //       style={{
+  //         backgroundImage:
+  //           "url(https://images.unsplash.com/photo-1590497008432-598f04441de8?q=80&w=2891&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+  //       }}
+  //     >
+  //       <div className="hero-overlay bg-opacity-40"></div>
+  //       <div className="hero-content text-center text-neutral-content">
+  //         <div className="max-w-md">
+  //           <h1 className="mb-4 text-4xl font-bold">
+  //             Check your Track & Trace
+  //           </h1>
+  //           {/* <input
+  //             type="text"
+  //             placeholder="Tracking number"
+  //             className="input input-bordered input-lg w-full max-w-md "
+  //           /> */}
+  //           <div className="form-control">
+  //             <div className="input-group">
+  //               <input
+  //                 type="text"
+  //                 placeholder="Search…"
+  //                 className="input input-bordered"
+  //               />
+  //               <button className="btn btn-square">
+  //                 <svg
+  //                   xmlns="http://www.w3.org/2000/svg"
+  //                   className="h-6 w-6"
+  //                   fill="none"
+  //                   viewBox="0 0 24 24"
+  //                   stroke="currentColor"
+  //                 >
+  //                   <path
+  //                     strokeLinecap="round"
+  //                     strokeLinejoin="round"
+  //                     strokeWidth="2"
+  //                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+  //                   />
+  //                 </svg>
+  //               </button>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
 
 export default App;
