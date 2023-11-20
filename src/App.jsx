5255a615f1f8c36ac4907fe603f83e7dcbd964ad
montiwat <monthiwat.a@sabuyinfra.com>
2023-11-20 11:44:20 +0700

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState, useRef } from 'react'
 import './App.css'
+import Search from './components/Search';
+import Tracking from './components/Tracking';
 
 function App() {
-  const [responseDataPostSabuy, setResponseDataPostSabuy] = useState(null);
-  const [responseDataNinja, setResponseDataNinja] = useState(null);
+  const [responseData, setResponseData] = useState(null);
   const [responseDataNotFound, setResponseDataNotFound] = useState(null);
 
   const refInput = useRef(null)
@@ -11,117 +12,19 @@ function App() {
   function handleSearchTrack () {
     const trackId = refInput.current.value;
 
-    if ( trackId.trim() !== '') {
-      // API Postsabuy
-      if (trackId.startsWith("E")) {
-        // Define the options for the fetch request
-        const requestOptionsPost = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          // body: JSON.stringify(postData),
-        };
-    
-        // Make the fetch request
-        fetch('https://postsabuy.info/Search?ConsignmentNo='+ trackId, requestOptionsPost)
-          .then(response => response.json())
-          .then(dataRes => {
-              console.log(dataRes);
-              const dataPostSabuy = {
-
-              }
-              setResponseDataPostSabuy(dataRes);
-              setResponseDataNinja("");
-              setResponseDataNotFound("");
-          })
-          .catch(error => {
-            // Handle errors here
-            console.error('Error fetching data:', error);
-          });    
-      } else if (trackId.startsWith("N")) {
-        //API ninja
-        // Define the options for the fetch request
-        const requestOptionsGet = {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          // body: JSON.stringify(postData),
-        };
-    
-        // Make the fetch request
-        fetch('https://walrus.ninjavan.co/th/dash/1.2/public/orders?tracking_id='+ trackId, requestOptionsGet)
-          .then(response => response.json())
-          .then(dataRes => {
-              console.log(dataRes);
-              setResponseDataNinja(dataRes);
-              setResponseDataPostSabuy("");
-              setResponseDataNotFound("");
-          })
-          .catch(error => {
-            // Handle errors here
-            console.error('Error fetching data:', error);
-          });    
+    fetch(`http://localhost:8000/api/tracking/${trackId}`)
+    .then(response => response.json())
+    .then(dataRes => {
+      console.log(dataRes);
+      if (dataRes.status === true) {
+        setResponseData(dataRes);
+        setResponseDataNotFound("");
       } else {
-        console.log("Setting Not Found ID");
-        setResponseDataNotFound("Setting Not Found ID: "+ trackId);
-        setResponseDataPostSabuy("");
-        setResponseDataNinja("");
+        console.log("Not Found naja")
+        setResponseData("");
+        setResponseDataNotFound(`ไม่พบหมายเลข ID: ${trackId}`);
       }
-  
-    }
-
-    // switch(true) {
-    //   case trackId.startsWith("EB") == true : 
-    //     // Define the options for the fetch request
-    //     const requestOptionsPost = {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       // body: JSON.stringify(postData),
-    //     };
-    
-    //     // Make the fetch request
-    //     fetch('https://postsabuy.info/Search?ConsignmentNo='+ trackId, requestOptionsPost)
-    //       .then(response => response.json())
-    //       .then(dataRes => {
-    //           console.log(dataRes);
-    //           setResponseData(dataRes);
-    //       })
-    //       .catch(error => {
-    //         // Handle errors here
-    //         console.error('Error fetching data:', error);
-    //       });
-    //       break;
-    //   case trackId.startsWith("EB") == true : 
-    //     // Define the options for the fetch request
-    //     const requestOptionsGet = {
-    //       method: 'GET',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       // body: JSON.stringify(postData),
-    //     };
-    
-    //     // Make the fetch request
-    //     fetch('https://walrus.ninjavan.co/th/dash/1.2/public/orders?tracking_id='+ trackId, requestOptionsGet)
-    //       .then(response => response.json())
-    //       .then(dataRes => {
-    //           console.log(dataRes);
-    //           setResponseData(dataRes);
-    //       })
-    //       .catch(error => {
-    //         // Handle errors here
-    //         console.error('Error fetching data:', error);
-    //       });
-    //       break;
-    //     default:
-    //       console.error('Invalid tracking ID prefix');
-    //       return;
-    // }
-
+    });
   }
 
   function handleKeyPress (e) {
@@ -132,65 +35,27 @@ function App() {
 
   function handleClearInput () {
     refInput.current.value = '';
-    setResponseDataPostSabuy("");
-    setResponseDataNinja("");
+    setResponseData("");
     setResponseDataNotFound("");
   }
 
   return (
     <>
-    <main className='flex min-h-screen flex-col items-center justify-center p-24 bg-gray-500'>
-      <div className='w-full h-screen rounded bg-white pt-10'>
+    <main className='flex min-h-screen flex-col items-center justify-center p-20 bg-white'>
+      <div className='w-full h-screen rounded bg-white pt-2'>
         <h2 className='text-center font-bold text-xl mb-2'>Tracking</h2>
-          <div className='flex items-center justify-center'>
-            <input className='m-1 p-1 bg-gray-50 border border-gray-300 text-gray-900'
-              type="text"
-              placeholder='Track ID'
-              ref={refInput}
-              onKeyDown={handleKeyPress}
-            />
-            <button onClick={handleSearchTrack} className='m-1 p-1  bg-red-600 border border-gray-300 text-white'>TRACK</button>
-            <button onClick={handleClearInput} className='m-1 p-1  bg-red-600 border border-gray-300 text-white'>CLEAR</button>
-          </div>
+        {/* SHOW  SEARCH INPUT*/}
+        <Search
+          refInput={refInput}
+          handleKeyPress={handleKeyPress}
+          handleSearchTrack={handleSearchTrack}
+          handleClearInput={handleClearInput}
+        />
         {/* SHOW TRACKING */}
-        <div className='grid items-center justify-center'>
-          {/* POSTSABUY */}
-          {responseDataPostSabuy && (
-            <>
-            <ul className='mt-5 mb-2'>
-              <li className='font-bold'>{responseDataPostSabuy.StatusDescription}</li>
-              <li>{responseDataPostSabuy.StatusDate}</li>
-            </ul>
-            {responseDataPostSabuy && responseDataPostSabuy.History.map((h, index) => (
-              <div key={index} className='ml-5 mb-2'>
-                <li className='font-bold'>{h.StatusDescription} - {h.LocationName}</li>
-                <p className='px-6'>{h.StatusDate}</p>
-              </div>
-            ))}            
-            </>
-          )}
-          {/* NINJA */}
-          {responseDataNinja && (
-            <>
-            <ul className='mt-5 mb-2'>
-              <li className='font-bold'>{responseDataNinja.status}</li>
-              <li>{responseDataNinja.delivery_end_date}</li>
-            </ul>
-            {responseDataNinja && responseDataNinja.events.map((ev, index) => (
-              <div key={index} className='ml-5 mb-2'>
-                <li className='font-bold'>{ev.type} - {ev.data.hub_name}</li>
-                <p className='px-6'>{ev.time}</p>
-              </div>
-            ))}            
-            </>
-          )}
-          {/* ID not found */}
-          <div className="py-10">
-            {responseDataNotFound && (
-              <h2>{responseDataNotFound}</h2>
-            )}
-          </div>
-        </div>
+        <Tracking
+          responseData={responseData}
+          responseDataNotFound={responseDataNotFound}
+        />
       </div>
     </main>
      
---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../App.css"
+
+export default function Search({
+    refInput,
+    handleKeyPress,
+    handleSearchTrack,
+    handleClearInput,
+}) {
+  return (
+    <div className='flex items-center justify-center'>
+      <input className='w-64 m-1 p-1 bg-gray-50 border border-gray-300 text-gray-900'
+        type="text"
+        placeholder='Track ID'
+        ref={refInput}
+        onKeyDown={handleKeyPress}
+      />
+      <button onClick={handleSearchTrack} className='m-1 p-1  bg-red-600 border border-gray-300 text-white'>TRACK</button>
+      <button onClick={handleClearInput} className='m-1 p-1  bg-red-600 border border-gray-300 text-white'>CLEAR</button>
+    </div>
+  )
+}
