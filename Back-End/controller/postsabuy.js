6fb28e213c107f938d1c61943f6baa54f535ff88
montiwat <monthiwat.a@sabuyinfra.com>
2023-11-27 14:21:01 +0700

    change fetch to axios

--- Back-End/controller/postsabuy.js
+++ Back-End/controller/postsabuy.js
@@ -1,42 +1,46 @@
 const axios = require("axios");
 
-// const getTrackingPostSabuyData = async (trackId) => {
-//     try {
-//         const trackingData = await axios.post(
-//             `https://postsabuy.info/Search?ConsignmentNo=${trackId}`,
-//             {
-//               "Content-Type": "application/json",
-//             }
-//         );
-//         return trackingData.data;
-//     } catch (error) {
-//         console.log("getTrackingPostSabuyData error : ", error.message);
-//         return error.message;
-        
-//     }
-// };
-
 const getTrackingPostSabuyData = async (trackId) => {
-    try {
-      const res = await fetch(`https://postsabuy.info/Search?ConsignmentNo=${trackId}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const trackingData = await res.json();
-      return trackingData;
-    } catch (error) {
-        if (error.response && error.response.data) {
-            console.log("getTrackingKerryExpressData error : ", error.response.data);
-            return error.response.data;
-          } else {
-            console.log("getTrackingKerryExpressData error : ", error.message);
-            return error.message;
-          }
+  try {
+    const trackingData = await axios.post(
+      `https://postsabuy.info/Search?ConsignmentNo=${trackId}`,
+      {
+        "Content-Type": "application/json",
+      }
+    );
+    return trackingData.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      console.log("getTrackingKerryExpressData error : ", error.response.data);
+      return error.response.data;
+    } else {
+      console.log("getTrackingKerryExpressData error : ", error.message);
+      return error.message;
     }
   }
+};
+
+// const getTrackingPostSabuyData = async (trackId) => {
+//     try {
+//       const res = await fetch(`https://postsabuy.info/Search?ConsignmentNo=${trackId}`, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       });
+//       const trackingData = await res.json();
+//       return trackingData;
+//     } catch (error) {
+//         if (error.response && error.response.data) {
+//             console.log("getTrackingKerryExpressData error : ", error.response.data);
+//             return error.response.data;
+//           } else {
+//             console.log("getTrackingKerryExpressData error : ", error.message);
+//             return error.message;
+//           }
+//     }
+//   }
 
 module.exports = {
-    getTrackingPostSabuyData,
-  };
+  getTrackingPostSabuyData,
+};
